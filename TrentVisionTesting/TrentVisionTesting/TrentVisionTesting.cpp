7c1e75cc2815b01297f2fbf3462167c7226e0295
Trent Rossos <31897216+trossossp3@users.noreply.github.com>
2019-01-14 17:34:19 -0500

    worked on more vision stuff

--- TrentVisionTesting/TrentVisionTesting/TrentVisionTesting.cpp
+++ TrentVisionTesting/TrentVisionTesting/TrentVisionTesting.cpp
@@ -28,46 +28,49 @@ int main() {
 
 	Mat imgRgb = imread("color.jpg");
 
+	
 	Mat imgHsv;
 	cvtColor(imgRgb, imgHsv, COLOR_BGR2HSV);
+	
+	//if (waitKey(30) == 27) 
+
 
 	for (;;) {
 		Mat imgGrey;
 		inRange(imgHsv, Scalar(hue_min, sat_min, val_min), Scalar(hue_max, sat_max, val_max), imgGrey);
-
+		;
 		Mat canny;
 		Canny(imgGrey, canny, 10, 100);
-
+		
 		vector<vector<Point	>> contours;
 		vector<Vec4i> hierarchy;
-
+		
 		findContours(canny, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE);
-
+		
 		vector<Rect> bounders;
 		
 		for (int i = 0; i < contours.size(); i++) {
 			int area = contourArea(contours[i]);
+			
 			if (area < max_area && area > min_area) {
 				bounders.push_back(boundingRect(contours[i]));
 			}
 		}
-
-
+		
 		
 		Mat imgBox = imgRgb.clone();
-		//for (int i = 0; i < bounders.size(); i++) {
-			Rect r1 = bounders[0];		
-			rectangle(imgBox, bounders[0], Scalar(0, 0, 255), 2);	
-
+		for (int i = 0; i < bounders.size(); i++) {
+			//cout << bounders.size();
+			//if (bounders.size() == 2) {
+			Rect r1 = bounders[0];
+			rectangle(imgBox, bounders[0], Scalar(0, 0, 255), 2);
 			circle(imgBox, Point(r1.x + r1.width / 2, r1.y + r1.height / 2), 10, Scalar(0, 0, 255), 2);
-
 			Rect r2 = bounders[1];
 			rectangle(imgBox, bounders[1], Scalar(0, 0, 255), 2);
-
 			circle(imgBox, Point(r2.x + r2.width / 2, r2.y + r2.height / 2), 10, Scalar(0, 0, 255), 2);
-			
+
 			//line(imgBox, Point(r2.x + r2.width / 2, r2.y + r2.height / 2), Point(r1.x + r1.width / 2, r1.y + r1.height / 2), Scalar(0, 0, 255));
-			
+
 
 			//finding the center of the two rectangles 
 			Point p1 = Point(r1.x + r1.width / 2, r1.y + r1.height / 2);
@@ -77,23 +80,25 @@ int main() {
 			double centerX = (r1.x + r1.width / 2 + r2.x + r2.width / 2) / 2;
 			//int totalWidth = GetSystemMetrics(SM_CXSCREEN);
 			int totalWidth = imgBox.size().width;
-			//cout << totalWidth + "                 ";
-			cout << centerX << endl;
+			//cout << totalWidth << endl;
+			//cout << centerX << endl;
 
 			circle(imgBox, center, 10, Scalar(0, 0, 255), 2);
 
 			//Yaw equation -((target-1/2*Totalx)/1/2*totalX)
 
-			double yaw = -1 * ((centerX - (0.5 * totalWidth) / (0.5 * totalWidth)));
-			//cout << yaw; 
-
-
+			double n1 = centerX - 0.5*totalWidth;
+			double n2 = 0.5*totalWidth;
+			int yaw = -1 * (n1 / n2);
+			cout << yaw << endl;
+		}
 		
 		//}
 	
 
-		//imshow("grey", imgBox);
-		//if (waitKey(30) == 27) break;
+		imshow("grey", imgBox);
+		if (waitKey(30) == 27) break;
+		
 	}
 
 }
