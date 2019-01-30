5583bf1a2d1067010635263122bb824b887ed8de
jslightham <31053827+jslightham@users.noreply.github.com>
2019-01-29 23:39:56 -0500

    Worked on PIDSubsystem Drivetrain

--- 2019RobotCode/src/main/java/frc/robot/subsystems/OldDriveTrain.java
+++ 2019RobotCode/src/main/java/frc/robot/subsystems/OldDriveTrain.java
@@ -6,34 +6,36 @@
 /*----------------------------------------------------------------------------*/
 
 package frc.robot.subsystems;
-
+import edu.wpi.first.wpilibj.command.Subsystem;
 import com.ctre.phoenix.sensors.PigeonIMU;
-import com.revrobotics.CANEncoder;
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
-import edu.wpi.first.wpilibj.command.PIDSubsystem;
+import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import frc.robot.RobotMap;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.revrobotics.CANEncoder;
 
 /**
  * Add your docs here.
  */
-public class DriveTrain extends PIDSubsystem {
+public class OldDriveTrain extends Subsystem {
+  private double kP = 0, kI = 0, kD = 0; 
   private PigeonIMU m_pidgey = new PigeonIMU(3); // 3 is can ID
+  private static CANSparkMax[] m_motors = new CANSparkMax[7];
+  private static CANEncoder[] m_encoders = new CANEncoder[m_motors.length];
+  private static SpeedControllerGroup m_leftMotorGroup, m_rightMotorGroup;
   private double[] ypr = new double[3];
-  private CANSparkMax[] m_motors = new CANSparkMax[7];
-  private CANEncoder[] m_motorEncoders = new CANEncoder[7];
-  private SpeedControllerGroup m_leftMotorGroup, m_rightMotorGroup;
   private DifferentialDrive m_driveTrain;
-  /**
-   * Add your docs here.
-   */
-  public DriveTrain() {
-    // Intert a subsystem name and PID values here
-    super("DriveTrain", 1, 2, 3);  // kp, ki, kd
-    setSetpoint(0);
-    enable();
+  private double Integral;
+  private double previousError = 0;
+
+
+  // Put methods for controlling this subsystem
+  // here. Call these from Commands.
+
+  public OldDriveTrain(){
+    super();
 
     m_motors[1] = new CANSparkMax(RobotMap.LEFT_MOTOR_1, MotorType.kBrushless);
     m_motors[2] = new CANSparkMax(RobotMap.LEFT_MOTOR_2, MotorType.kBrushless);
@@ -42,12 +44,11 @@ public class DriveTrain extends PIDSubsystem {
     m_motors[5] = new CANSparkMax(RobotMap.RIGHT_MOTOR_2, MotorType.kBrushless);
     m_motors[6] = new CANSparkMax(RobotMap.RIGHT_MOTOR_3, MotorType.kBrushless);
     for (int i = 0; i < m_motors.length; i++) {
-      m_motorEncoders[i] = new CANEncoder(m_motors[i]);
+      m_encoders[i] = new CANEncoder(m_motors[i]);
     }
     m_leftMotorGroup = new SpeedControllerGroup(m_motors[1], m_motors[2], m_motors[3]);
     m_rightMotorGroup = new SpeedControllerGroup(m_motors[4], m_motors[5], m_motors[6]);
     m_driveTrain = new DifferentialDrive(m_leftMotorGroup, m_rightMotorGroup);
-
   }
 
   @Override
@@ -56,18 +57,16 @@ public class DriveTrain extends PIDSubsystem {
     // setDefaultCommand(new MySpecialCommand());
   }
 
-  @Override
-  protected double returnPIDInput() {
-    // Return your input value for the PID loop
-    m_pidgey.getYawPitchRoll(ypr); // get yaw, pitch, roll and store in ypr array
-    // We should add something to check if the value we return makes sense.
-    return ypr[0];
+  public double PID(double target){
+    m_pidgey.getYawPitchRoll(ypr);
+    double error =  target - ypr[0];
+    double pTerm = error * kP;
+    Integral += error;
+    double iTerm = Integral * kI;
+    double dTerm = kD * (previousError - error);
+    previousError = error;
+    return pTerm + iTerm + dTerm;
   }
 
-  @Override
-  protected void usePIDOutput(double output) {
-    // Use output to drive your system, like a motor
-    // e.g. yourMotor.set(output);
-    
-  }
+  
 }
