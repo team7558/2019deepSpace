2206a2e8a5fd3d464e2ea5d240fc5fb55ac4a8b7
jslightham <31053827+jslightham@users.noreply.github.com>
2019-02-14 11:13:45 -0500

    initial php

---
+++ JohnathonAppPHP/get.php
@@ -0,0 +1,19 @@
+<?php 
+error_reporting(E_ERROR | E_PARSE);
+session_start();
+
+
+$sql = "INSERT INTO matches () VALUES ()";
+
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+</body>
+</html>
